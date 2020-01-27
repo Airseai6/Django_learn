6f97b2b9a276f777fbd4b7410d108b74785e2add
littleStone <1042536516@qq.com>
2020-01-27 23:57:33 +0800

    Signed-off-by: littleStone <1042536516@qq.com>

--- django_demo07/s4day76/app01/views.py
+++ django_demo07/s4day76/app01/views.py
@@ -9,7 +9,7 @@ def test(request):
 
 class LoginFrom(Form):
     # 正则的验证
-    username = fields.CharField(max_length=18, min_length=6, required=True)
+    username = fields.CharField(max_length=18, required=True)
     # 正则的验证
     password = fields.CharField(max_length=16, required=True)
 
@@ -25,11 +25,43 @@ def login(request):
         # else:
         #     return render(request, 'login.html', {'msg': 'username or pwd is error !'})
         obj = LoginFrom(request.POST)
+        """
+        1. LoginForm实例化时
+             self.fields={
+                 'username':正则表达式，
+                 'password':正则表达式，
+             }
+        2. 循环self.fields
+            for k, v in self.fields.items():
+                k是：username，password
+                v是：正则表达式
+                input_value = request.POST.get(k)
+        """
         if obj.is_valid():
+            print('****************')
             print(obj.cleaned_data) # 字典类型
+            print('****************')
             # .create(**obj.cleaned_data)
             return HttpResponse('ok')
         else:
+            print('****************')
             print(obj.errors) # __str__对象
+            print('****************')
             return render(request, 'login.html', {'obj':obj})
 
+def ajax_login(request):
+    import json
+    ret = {'status': True, 'msg': None}
+    obj = LoginFrom(request.POST)
+    if obj.is_valid():
+        print('****************')
+        print(obj.cleaned_data)
+        print('****************')
+    else:
+        # print('****************')
+        # print(obj.errors)
+        # print('****************')
+        ret['status'] = False
+        ret['msg'] = obj.errors
+        v = json.dumps(ret)
+    return HttpResponse(v)
