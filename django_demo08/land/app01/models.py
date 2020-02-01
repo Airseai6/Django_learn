ca8fa18c49570bdd4da08427df406035104fd52f
littleStone <1042536516@qq.com>
2020-02-02 00:30:39 +0800

    有地图显示 Signed-off-by: littleStone <1042536516@qq.com>

--- django_demo08/land/app01/models.py
+++ django_demo08/land/app01/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+# import time
+import json
+import requests
 
-# Create your models here.
+
+# def get_data():
+#     url = 'https://view.inews.qq.com/g2/getOnsInfo?name=wuwei_ww_area_counts&callback=&_=%d' % int( time.time()*1000)
+#     data = json.loads(requests.get(url=url).json()['data'])
+
+#     mapAreaList = ["南海诸岛", '北京', '天津', '上海', '重庆', '河北', '河南', '云南', '辽宁', '黑龙江', '湖南', '安徽', '山东', '新疆', '江苏', '浙江', '江西', '湖北', '广西', '甘肃', '山西', '内蒙古', '陕西', '吉林', '福建', '贵州', '广东', '青海', '西藏', '四川', '宁夏', '海南', '台湾', '香港', '澳门', ]
+
+#     # {'country': '中国', 'area': '广东', 'city': '广州', 'confirm': 94, 'suspect': 0, 'dead': 0, 'heal': 0}
+#     areaData = {}
+#     confirmData = {}
+#     for item in data:
+#         if item['country'] == '中国':
+#             if item['area'] in confirmData:
+#                 areaData[item['area']].append(item)
+#                 confirmData[item['area']] += item['confirm']
+#             else:
+#                 areaData[item['area']] = [item,]
+#                 confirmData[item['area']] = item['confirm']
+    
+#     reConfirmData = {}
+#     for area in mapAreaList:
+#         if area in confirmData:
+#             reConfirmData[area] = confirmData[area]
+#         else:
+#             reConfirmData[area] = 0
+
+#     return [areaData, reConfirmData]
+
+def get_data():
+    url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5'
+    data = json.loads(requests.get(url=url).json()['data'])
+
+    # print(data['areaTree'][0]['children'][0]['name'])
+    # print(data['areaTree'][0]['children'][0]['total']['confirm'])
+
+    # dataDic = {'南海诸岛':0,}
+    # for i in range(34):
+    #     dataDic[data['areaTree'][0]['children'][i]['name']] = data['areaTree'][0]['children'][i]['total']['confirm']
+
+    return data
--- django_demo08/land/app01/views.py
+++ django_demo08/land/app01/views.py
@@ -1,6 +1,8 @@
 from django.forms import Form, fields
 from django.shortcuts import render, HttpResponse, redirect
 
+from . import models
+
 
 class LoginFrom(Form):
     username = fields.CharField(max_length=18, required=True)
@@ -13,13 +15,16 @@ def login(request):
     else:
         obj = LoginFrom(request.POST)
         if obj.is_valid():
-            if obj.cleaned_data['username'] == 'root' and obj.cleaned_data['password'] == '123':
-                ret = redirect('/index/')
-                ret.set_signed_cookie('username', obj.cleaned_data['username'], salt='123')
-                return ret
-            else:
-                msg_err = 'Incorrect user name or password.'
-                return render(request, 'login.html', {'msg_err': msg_err})
+            # if obj.cleaned_data['username'] == 'root' and obj.cleaned_data['password'] == '123':
+            #     ret = redirect('/index/')
+            #     ret.set_signed_cookie('username', obj.cleaned_data['username'], salt='123')
+            #     return ret
+            # else:
+            #     msg_err = 'Incorrect user name or password.'
+            #     return render(request, 'login.html', {'msg_err': msg_err})
+            ret = redirect('/index/')
+            ret.set_signed_cookie('username', obj.cleaned_data['username'], salt='123')
+            return ret
         else:
             return render(request, 'login.html', {'obj': obj})
 
@@ -33,6 +38,7 @@ def logout(request):
 def index(request):
     username = request.get_signed_cookie('username', default=0, salt='123')
     if username:
-        return render(request, 'index.html', {'username': username,})
+        dataRet = models.get_data()
+        return render(request, 'index.html', {'username': username, 'dataRet': dataRet},)
     else:
         return redirect('/login/')
